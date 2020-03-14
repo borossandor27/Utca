50449be7312865e533b8ef5f74684e104618e872
borossandor27 <boros.sanyi@gmail.com>
2020-03-14 21:59:25 +0100

    Add project files.

---
+++ Utca/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Utca
+{
+    class Program
+    {
+        public static readonly char FESTETLEN_KERITES = '#';
+        public static readonly char NINCS_KERITES = ':';
+        public static readonly int PAROS = 0;
+        public static readonly int PARATLAN = 1;
+
+        static List<Telek> telkek = new List<Telek>();
+
+        static void Main(string[] args)
+        {
+            Beolvas(@"..\..\kerites.txt");
+            Console.WriteLine("\n2. feladat:");
+            Console.WriteLine($"Az eladott telkek száma: {telkek.Count}");
+
+            Console.WriteLine("\n3. feladat:");
+            if (telkek[telkek.Count-1].Oldal == PAROS)
+            {
+                Console.WriteLine("\ta. A páros oldalon adták el az utolsó telket.");
+            }
+            else
+            {
+                Console.WriteLine("\ta. A páratlan oldalon adták el az utolsó telket.");
+            }
+            Console.WriteLine($"\tb. Az utolsó telek házszáma: {telkek[telkek.Count - 1].Hazszam}");
+
+            Console.WriteLine("\n4. feladat");
+            List<Telek> paratlanok = telkek.FindAll(a => a.Oldal == PARATLAN).ToList();
+            for (int i = 0; i < paratlanok.Count-1; i++)
+            {
+                if (paratlanok[i].Kerites.Equals(paratlanok[i+1].Kerites) && paratlanok[i].Kerites != FESTETLEN_KERITES && paratlanok[i].Kerites != NINCS_KERITES)
+                {
+                    Console.WriteLine($"\tA szomszédossal egyezik a kerítés színe: {paratlanok[i].Hazszam}");
+                    break;
+                }
+            }
+
+            Console.WriteLine("\n5. feladat");
+            Console.Write("\tAdjon meg egy házszámot! ");
+            int.TryParse(Console.ReadLine(), out int hazszam);
+            Telek telek = telkek.Find(a => a.Hazszam == hazszam);
+            Console.WriteLine($"\tA kerítés színe / állapota: {telek.Kerites}");
+            char szomszed1 = telkek.Find(a => a.Hazszam == hazszam - 2).Kerites;
+            char szomszed2 = telkek.Find(a => a.Hazszam == hazszam + 2).Kerites;
+            Random r = new Random();
+            char ujszin = '\0';
+            do
+            {
+                ujszin = (char)('A' + r.Next('Z' - 'A'));
+            } while (ujszin.Equals(szomszed1) || ujszin.Equals(szomszed2));
+            Console.WriteLine($"\tEgy lehetséges festési szín: {ujszin}");
+            Console.WriteLine("\nProgram vége!");
+            Console.ReadKey();
+        }
+        static void Beolvas(string forras)
+        {
+            try
+            {
+                using (StreamReader sr = new StreamReader(forras))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        telkek.Add(new Telek(sr.ReadLine().Split()));
+                    }
+                }
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine(ex.Message);
+                Environment.Exit(0);
+            }
+        }
+    }
+}
---
+++ Utca/Telek.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utca
+{
+    class Telek
+    {
+        int oldal;      //-- a telek a páros(0) vagy a páratlan(1) oldalán van az utcának;
+        int szelesseg;  //-- a telek szélességét adja meg méterben(egész szám, értéke 8 és 20 között lehet);
+        char kerites;   //-- 
+        int hazszam;
+        static int paros = 2;  //-- kezdő házszám a páros oldalon
+        static int paratlan = 1;   //-- kezdő házszám a páratlan oldalon
+
+        public int Oldal { get => oldal; set => oldal = value; }
+        public int Szelesseg { get => szelesseg; set => szelesseg = value; }
+        public char Kerites { get => kerites; set => kerites = value; }
+        public int Hazszam { get => hazszam; set => hazszam = value; }
+
+        public Telek(string[] sor)
+        {
+            this.oldal = int.Parse(sor[0].Trim());
+            if (oldal == Program.PAROS)
+            {
+                this.hazszam = paros;
+                paros += 2;
+            }
+            else
+            {
+                this.hazszam = paratlan;
+                paratlan += 2;
+            }
+            this.szelesseg = int.Parse(sor[1].Trim());
+            this.kerites = sor[2].Trim()[0];
+        }
+    }
+}
